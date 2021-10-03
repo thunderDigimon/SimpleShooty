0fdb94381344704f71c5fb5f373a7045971ab978
yathishgk <>
2021-10-03 11:48:33 +0530

    camera fit

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,19 +1,32 @@
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour {
+public class CameraFollow : MonoBehaviour
+{
 
-	public Transform target;
+    public Transform target;
 
-	public float smoothSpeed = 0.125f;
-	public Vector3 offset;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    public float horizontalFoV = 70.0f;
 
-	void FixedUpdate ()
-	{
-		Vector3 desiredPosition = target.position + offset;
-		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-		transform.position = smoothedPosition;
+    void Start()
+    {
+        UpdateFOV();
+    }
 
-		// transform.LookAt(target);
-	}
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+
+    void UpdateFOV()
+    {
+        float halfWidth = Mathf.Tan(0.5f * horizontalFoV * Mathf.Deg2Rad);
+        float halfHeight = halfWidth * Screen.height / Screen.width;
+        float verticalFoV = 2.0f * Mathf.Atan(halfHeight) * Mathf.Rad2Deg;
+        Camera.main.fieldOfView = verticalFoV;
+    }
 
 }
